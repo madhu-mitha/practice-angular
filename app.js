5b85cd7c3c2371476c1743b3bd9e25812ca340a0
MithaMadhuS <madhumitha.s@hashedin.com>
2018-08-16 19:22:01 +0530

    dependency injection

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
     'use strict';
     angular.module("myApp",[])
 
-    .controller("myController", function($scope){
+    .controller("myController",myController); 
+
+    function myController($scope,$filter,$injector){
         $scope.firstname="Madhu";
         $scope.lastname="Mitha";
         $scope.fullname = function(){
             return $scope.firstname+" "+$scope.lastname;
         };
 
-    });
+        $scope.upper=function(){
+            var upCase= $filter('uppercase');
+            $scope.firstname= upCase($scope.firstname);
+        }
+
+        console.log($injector.annotate(myController));
+    }
 
 })();
